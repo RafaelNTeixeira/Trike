797ee181b2687cc3e18ab791b6d6e99b2edf5d66
RafaelNTeixeira <rafael2003t.18@gmail.com>
2023-10-26 23:19:45 +0100

    Player Vs Player (Almost Done - Needs end condition debugging)

--- src/display.pl
+++ src/display.pl
@@ -16,41 +16,20 @@ initial_state(Size, GameState) :-
     ],
     assertz(size(Size)).
 
-/*
-print_board(_, Lines, TotalLines) :- Lines > TotalLines. 
-print_board([Line | Matrix], Lines, TotalLines) :-
-    write('----|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|'), nl,
-    write('<'), write(Lines), write('> | '),
-    print_line(Line), nl,
-    NextLines is Lines + 1,
-    print_board(Matrix, NextLines, TotalLines).
+display_game_pie_rule(Board) :-
+    nl,
+    write('  X\n'), 
+    write('----|---|---|---|---|---|---|---|'), nl,
+    display_rows(Board, 0),
+    write('    | 0 | 1 | 2 | 3 | 4 | 5 | 6 | Y\n'), nl, nl.
 
 display_game([Player|Board]) :-
-    nl,
-    size(Size),
-    TotalLines is Size - 1, 
-    print_board(Board, 0, TotalLines),
-    write('----|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|'), nl,
-    write('    | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11| 12| 13| 14|\n'), nl, nl,
-    write('    > Turn of Player '),
-    identity(Player, Identity), 
-    write(Identity), write(' <'), nl, nl.
-
-print_line([]).
-print_line([Ele|RestLine]) :-
-    identity(Ele, Identity),
-    write(Identity),
-    write(' | '),
-    print_line(RestLine).
-*/
-
-display_board(Board) :-
     nl,
     write('  X\n'), 
     write('----|---|---|---|---|---|---|---|'), nl,
     display_rows(Board, 0),
     write('    | 0 | 1 | 2 | 3 | 4 | 5 | 6 | Y\n'), nl, nl,
-    nl.
+    write('Player '), write(Player), write(' turn'), nl, nl.
 
 display_rows([], _).
 display_rows([Row | Rest], N) :-
--- src/logic.pl
+++ src/logic.pl
@@ -1,12 +1,13 @@
 initial_player(b).
+other_player(w).
 
 switch_player(b, w).
 switch_player(w, b).
 
 print_indication :-
     write('============================================'), nl,
-    write('Player b = Player with the black pieces(b)'), nl,
-    write('Player w = Player with the white pieces(w)'), nl,
+    write('Player b = Player with the black pieces (b)'), nl,
+    write('Player w = Player with the white pieces (w)'), nl,
     write('============================================'), nl, nl,
     write('\nPlayer 1 starts with the black pieces\n'),
     write('Player 2 starts with the white pieces\n'), nl.
@@ -14,23 +15,34 @@ print_indication :-
 % play_game will receive Level variable
 play_game :- 
     initial_state(8, GameState),
-    initial_player(Player), nl, 
+    initial_player(Black), nl, 
+    other_player(White),
     print_indication,
-    pie_rule(GameState, NewGameState),
-    % gameplay(NewGameState, Player, FinalScore), % play_game
+    pie_rule(GameState, PlayerPos, NewGameState),
+    gameplay(NewGameState, PlayerPos, FinalScore), % play_game
     write('gameplay\n'),
     % report_winner(FinalScore),
     write('report_winner\n').
 
-gameplay(GameState, FinalScore) :-
-    (game_over(GameState) -> calculate_final_score(GameState, FinalScore);
-        move(GameState, Move, NewGameState), % move chama valid_moves(+GameState, +Player, -ListOfMoves) ou chama-se aqui e adiciona-se Player como argumento em gameplay ???
-        gameplay(NewGameState, FinalScore)
+gameplay([Player|Board], PlayerPos, FinalScore) :-
+    (game_over([Player|Board]) -> calculate_final_score([Player|Board], FinalScore);
+        valid_moves([Player|Board], PlayerPos, ListOfMoves),
+        write('Player '), write(Player), write(', choose an X starting point:'),
+        read(PointX),
+        write('Player '), write(Player), write(', now choose an Y starting point: '),
+        read(PointY),
+        Move = ([PointX, PointY]),
+        move([Player|ListOfMoves], Move, [NewPlayer|NewBoard]), 
+        gameplay([NewPlayer|NewBoard], Move, FinalScore) 
     ).
+
+valid_moves([CurPlayer|Board], [PlayerX, PlayerY], ListOfMoves) :-
+    swap(PlayerX, PlayerY, Board, ListOfMoves),
+    display_game([CurPlayer|ListOfMoves]).
 
 calculate_final_score([Player|Board], Score) :-
     findall(Point, adjacent_or_under(Board, 0, 0, Player, Point), Points),
-    length(Points, Score).
+    length(Points, Score). % guarda em score o tamanho de Points (peças que servem para pontuação)
 
 game_over([LastPlayer|Board]) :-
     \+ can_move(Board, 0, 0), % If the pawn can't move from the center, it's trapped.
@@ -48,28 +60,39 @@ checkers_around(Board, X, Y, Player, Checkers) :-
     length(Checkers, Count),
     Count >= 2. % There must be at least 2 adjacent checkers to make a move.
     
-move([Player|Board], Move, [NewPlayer|NewBoard]) :- % need to add Move argument and NewPlayer
-    display_board(Board),
-    write('Player '), write(Player), write(', make a move (X Y): '),
-    read(PointX), read(PointY),
-    (valid_move(Board, Player, PointX, PointY) ->   % valid_moves(+GameState, +Player, -ListOfMoves)
-        update_board(Board, Player, PointX, PointY, NewBoard),
-        switch_player(NewPlayer, Player);
+move([Player|Board], [PointX, PointY], [NewPlayer|NewBoard]) :- 
+    (check_if_valid(Board, PointX, PointY) ->
+        replace(Board, PointX, PointY, Player, TempBoard),
+        clean_playables(TempBoard, NewBoard),
+        switch_player(NewPlayer, Player),
+        display_game([NewPlayer|NewBoard]); % tem que ser display do board sem os ps
         write('Invalid move. Try again.\n'),
-        move(Board, Player, NewBoard)
+        write('Player '), write(Player), write(', choose an X starting point:'),
+        read(PointX),
+        write('Player '), write(Player), write(', now choose an Y starting point: '),
+        read(PointY),
+        Move = [PointX, PointY],
+        move(Board, Move, NewBoard)
     ).
+
+% Checks if a point is playable.
+check_if_valid(Board, X, Y) :-
+    is_inside(Board, X, Y),
+    nth0(X, Board, Row),
+    nth0(Y, Row, p).
 
 update_board_first_play(Board, Player, PointX, PointY, NewBoard) :-
     replace(Board, PointX, PointY, Player, NewBoard),
-    display_board(NewBoard).
+    display_game_pie_rule(Board).
 
 update_board(Board, Player, PointX, PointY, NewBoard) :-
     replace(Board, PointX, PointY, Player, TempBoard),
-    display_board(TempBoard),
-    move_pawn(TempBoard, PointX, PointY, NewBoard, Opponent),
+    % print do board sem as jogaveis possiveis e com a nova peça inserida
+    display_game([Player|Board]),
+    % move_pawn(TempBoard, PointX, PointY, NewBoard, Opponent), % é preciso???
     write('move_pawn\n'),
-    display_board(NewBoard),
-    write('display_board\n').
+    % display_game([Board|TempBoard]),
+    write('display_game\n').
 
 % Replace a cell in the board with a new value.
 replace([Row | Rest], 0, Y, NewValue, [NewRow | Rest]) :-
@@ -80,7 +103,7 @@ replace([Row | Rest], X, Y, NewValue, [Row | NewRest]) :-
     replace(Rest, X1, Y, NewValue, NewRest).
 
 replace_in_row([_ | Rest], 0, Y, NewValue, [NewValue | Rest]) :-
-    Y = 0.
+    Y = 0; Y = p.
 replace_in_row([Value | Rest], X, Y, NewValue, [Value | NewRest]) :-
     Y > 0,
     Y1 is Y - 1,
@@ -113,30 +136,30 @@ jump_over_checkers(Board, X, Y, NewX, NewY) :-
     is_empty(Board, X1, Y1),
     jump_over_checkers(Board, X1, Y1, NewX, NewY).
 
-pie_rule([Player|Board], [NextPlayer|NewBoard]) :-
-    display_board(Board),
+pie_rule([Player|Board], PlayerPos, [CurPlayer|NewBoard]) :-
+    display_game_pie_rule(Board),
+    write('Player 1\'s turn:'), nl, nl,
     write('Player '), write(Player), write(', choose an X starting point:'),
     read(PointX),
     write('Player '), write(Player), write(', now choose an Y starting point: '),
     read(PointY),
     (is_empty(Board, PointX, PointY) ->
-        replace(Board, PointX, PointY, Player, TempBoard),
+        replace(Board, PointX, PointY, Player, TempBoard), % Player = b
         nl, nl,
         write('Player 1\'s play:\n'),
-        display_board(TempBoard),
+        display_game_pie_rule(TempBoard),
         write('Player Whites, do you want to switch colors?\n'),
         write('1. Yes'), nl, write('2. No'), nl,
         read(Choice),
-        (Choice =:= 1 -> CurPlayer = b; Choice =:= 2 -> CurPlayer = w; CurPlayer = w), % fazer função que faz a troca. Repetir sempre se n escolher o número válido
-        nl,
+        CurPlayer = w,
         write('\nPlayer 2\'s play:\n'),
-        update_board_first_play(TempBoard, CurPlayer, PointX, PointY, NewBoard),
-        NextPlayer = w,
+        update_board_first_play(TempBoard, Player, PointX, PointY, NewBoard),
         (Choice =:= 1 -> 
             write('\nPlayer 1 is now playing with the white pieces\n'),
-            write('Player 2 is now playing with the black pieces\n'), nl; true);
+            write('Player 2 is now playing with the black pieces\n'), nl; true),
+        PlayerPos = [PointX, PointY];
         write('Invalid choice. Try again.\n'),
-        pie_rule([Player|Board], [NPlayer|NBoard])
+        pie_rule([Player|Board], PlayerPos, [CurPlayer|NBoard])
     ).
 
 % Checks if a point is empty.
@@ -181,11 +204,11 @@ adjacent_or_under(Board, X, Y, Player, Point) :-
     under(Board, X, Y, Player, Point).
 
 % Check if a point is adjacent to the given player checker.
-adjacent(Board, X, Y, Player, Point) :-
+adjacent(Board, X, Y, Player, Point) :- % pode n estar correto as coordenadas. Objetivo é verificar numa row se tem lá o símbolo do player e guardar esse ponto
     is_adjacent(X, Y, AdjX, AdjY),
     is_inside(Board, AdjX, AdjY),
-    nth0(AdjY, Board, Row),
-    nth0(AdjX, Row, Player),
+    nth0(AdjX, Board, Row),
+    nth0(AdjY, Row, Player),
     Point = [AdjX, AdjY].
 
 is_adjacent(X1, Y1, X2, Y2) :-
@@ -195,12 +218,12 @@ is_adjacent(X1, Y1, X2, Y2) :-
     (X1 =:= X2 + 1, Y1 =:= Y2).
 
 % Check if a point is under the given player checker.
-under(Board, X, Y, Player, Point) :-
-    Y1 is Y + 1,
-    is_inside(Board, X, Y1),
-    nth0(Y1, Board, Row),
-    nth0(X, Row, Player),
-    Point = [X, Y1].
+under(Board, X, Y, Player, Point) :- % pode n estar correto. Objetivo é verificar se em rows em baixo encontra-se uma peça do player especificado
+    X1 is X + 1,
+    is_inside(Board, X1, Y),
+    nth0(X1, Board, Row),
+    nth0(Y, Row, Player),
+    Point = [X1, Y].
 
 % Report the winner of the game.
 report_winner(Score) :-
@@ -211,3 +234,259 @@ report_winner(Score) :-
         write(' points wins the game.\n');
         write('It\'s a draw! No one wins.')
     ).
+
+% ----------------------------------------------------------------------------------------------------------------------------
+/*
+[
+    [0],
+    [0,0],
+    [0,0,0],
+    [0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0], 
+    [0,0,0,0],
+    [0,0,0],
+    [0,0],
+    [0]
+]
+
+[[b],[0, 0],[0, p, 0],[0, p, 0, 0],[p, 0, b, 0, b],[0, p, 0, b, 0, p],[0, 0, 0, 0, 0, 0, p],[w, p, w, 0, 0, p],[0, 0, w, p, 0],[0, p, p, 0],[0, p, 0],[0, p],[p]]
+
+*/
+
+/*------------------------------------------------------------------------------------*/
+print_list([]).
+print_list([H|T]) :-
+    write(H),           % Print the current element
+    write(' '),                 % Newline for formatting
+    print_list(T).       % Recursively print the rest of the list
+
+/*------------------------------------------------------------------------------------*/
+print_board([]).
+print_board([Row|Rest]) :-
+    print_row(Row),
+    nl,  % New line for the next row
+    print_board(Rest).
+
+print_row([]).
+print_row([X|Rest]) :-
+    write(X),  % Print the current element
+    write(' '),  % Add a space between elements for formatting
+    print_row(Rest).
+
+/*------------------------------------------------------------------------------------*/
+
+% custom_nth1(+Index, +List, -Element)
+custom_nth1(0, [Element|_], Element).
+custom_nth1(Index, [_|Rest], Element) :-
+    Index > 0,
+    NextIndex is Index - 1,
+    custom_nth1(NextIndex, Rest, Element).
+
+/*------------------------------------------------------------------------------------*/
+
+elem_belongs_to_both(Elem) :-
+    Elem = b,
+    Elem = w.
+
+/*------------------------------------------------------------------------------------*/
+% Swap 0 with p in all directions from X
+swap(Row, Col, Board, NewBoard) :-
+    % swapRight(Row, Col, Board, NewBoard),
+    % print_board(NewBoard).
+    swapInDirection(Row, Col, Board, NewBoard).
+
+% Swap 0 with p in all directions (up, down, left, right, and diagonals)
+swapInDirection(Row, Col, Board, NewBoard) :-
+    swapUp(Row, Col, Board, TempBoard1),
+    swapDown(Row, Col, TempBoard1, TempBoard2),
+    swapLeft(Row, Col, TempBoard2, TempBoard3),
+    swapRight(Row, Col, TempBoard3, TempBoard4),
+    swapDiagonal1(Row, Col, TempBoard4, TempBoard5),
+    swapDiagonal2(Row, Col, TempBoard5, TempBoard6),
+    swapDiagonal3(Row, Col, TempBoard6, TempBoard7),
+    swapDiagonal4(Row, Col, TempBoard7, NewBoard).
+    % print_board(NewBoard).
+
+/*---------------- Swap Up -------------------------*/
+% Swap 0 with p in the up direction
+swapUp(Col, Col, Board, NewBoard) :-
+    NewBoard = Board.
+
+swapUp(Row, Col, Board, NewBoard) :-
+    RowAbove is Row - 1,
+    custom_nth1(RowAbove, Board, RowAboveList),
+    custom_nth1(Col, RowAboveList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board; % Stop the loop if Elem is 'w' or 'b'
+    (Elem = 0) ->
+        replace_swap(Col, RowAboveList, p, NewRowAbove),
+        replace_swap(RowAbove, Board, NewRowAbove, TempBoard),
+        swapUp(RowAbove, Col, TempBoard, NewBoard);
+    NewBoard = Board). % Default case when Elem is something else, no change
+
+/*---------------- Swap Down -------------------------*/
+% Swap 0 with p in the down direction
+swapDown(Row, Col, Board, NewBoard) :-
+    Row + Col =:= 12,
+    NewBoard = Board.
+
+swapDown(Row, Col, Board, NewBoard) :-
+    RowBelow is Row + 1,
+    custom_nth1(RowBelow, Board, RowBelowList),
+    custom_nth1(Col, RowBelowList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board; % Stop the loop if Elem is 'w' or 'b'
+    (Elem = 0) -> 
+        replace_swap(Col, RowBelowList, p, NewRowBelow),
+        replace_swap(RowBelow, Board, NewRowBelow, TempBoard),
+        swapDown(RowBelow, Col, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*---------------- Swap Left -------------------------*/
+% Swap 0 with p in the left direction
+swapLeft(Row, 0, Board, NewBoard) :-
+    NewBoard = Board.
+
+swapLeft(Row, Col, Board, NewBoard) :-
+    custom_nth1(Row, Board, RowList),
+    ColLeft is Col - 1,
+    custom_nth1(ColLeft, RowList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board; % Stop the loop if Elem is 'w' or 'b'
+    (Elem = 0) -> 
+        replace_swap(ColLeft, RowList, p, NewRowLeft),
+        replace_swap(Row, Board, NewRowLeft, TempBoard),
+        swapLeft(Row, ColLeft, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*---------------- Swap Right -------------------------*/
+% Swap 0 with p in the right direction
+swapRight(Row, Row, Board, NewBoard) :-
+    NewBoard = Board.
+
+swapRight(Row, Col, Board, NewBoard) :-
+    Row + Col =:= 12,
+    NewBoard = Board.
+
+swapRight(Row, Col, Board, NewBoard) :-
+    custom_nth1(Row, Board, RowList),
+    ColRight is Col + 1,
+    custom_nth1(ColRight, RowList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board; % Stop the loop if Elem is 'w' or 'b'
+    (Elem = 0) ->  
+        replace_swap(ColRight, RowList, p, NewRowRight),
+        replace_swap(Row, Board, NewRowRight, TempBoard),
+        swapRight(Row, ColRight, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*---------------- Swap Diagonal1 (Up and Left)-------------------------*/
+% Swap 0 with p in the first diagonal direction (top-left to bottom-right)
+swapDiagonal1(Row, 0, Board, NewBoard) :-
+    NewBoard = Board.
+
+swapDiagonal1(Row, Col, Board, NewBoard) :-
+    RowAbove is Row - 1,
+    ColLeft is Col - 1,
+    custom_nth1(RowAbove, Board, RowAboveList),
+    custom_nth1(ColLeft, RowAboveList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board; % Stop the loop if Elem is 'w' or 'b'
+    (Elem = 0) ->  
+        replace_swap(ColLeft, RowAboveList, p, NewTopLeft),
+        replace_swap(RowAbove, Board, NewTopLeft, TempBoard),
+        swapDiagonal1(RowAbove, ColLeft, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*---------------- Swap Diagonal2 (Low and Left)-------------------------*/
+% Swap 0 with p in the second diagonal direction (top-right to bottom-left)
+swapDiagonal2(Row, 0, Board, NewBoard) :-
+    NewBoard = Board.
+
+swapDiagonal2(Row, Col, Board, NewBoard) :-
+    RowBelow is Row + 1,
+    ColLeft is Col - 1,
+    custom_nth1(RowBelow, Board, RowBelowList),
+    custom_nth1(ColLeft, RowBelowList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board; % Stop the loop if Elem is 'w' or 'b'
+    (Elem = 0) ->  
+        replace_swap(ColLeft, RowBelowList, p, NewBottomLeft),
+        replace_swap(RowBelow, Board, NewBottomLeft, TempBoard),
+        swapDiagonal2(RowBelow, ColLeft, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*---------------- Swap Diagonal3 (Up and Right)-------------------------*/
+swapDiagonal3(Col, Col, Board, NewBoard) :-
+    NewBoard = Board.
+
+swapDiagonal3(Row, Col, Board, NewBoard) :-
+    Row - Col =:= 1,
+    NewBoard = Board.
+
+swapDiagonal3(Row, Col, Board, NewBoard) :-
+    RowAbove is Row - 1,
+    ColRight is Col + 1, 
+    custom_nth1(RowAbove, Board, RowAboveList),
+    custom_nth1(ColRight, RowAboveList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board;
+    (Elem = 0) ->  
+        replace_swap(ColRight, RowAboveList, p, NewTopRight),
+        replace_swap(RowAbove, Board, NewTopRight, TempBoard),
+        swapDiagonal3(RowAbove, ColRight, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*---------------- Swap Diagonal4 (Low and Right)-------------------------*/
+swapDiagonal4(Row, Col, Board, NewBoard) :-
+    Row + Col =:= 12,
+    NewBoard = Board.
+
+swapDiagonal4(Row, Col, Board, NewBoard) :-
+    Row + Col =:= 11,
+    NewBoard = Board.
+
+swapDiagonal4(Row, Col, Board, NewBoard) :-
+    RowBelow is Row + 1,
+    ColRight is Col + 1,
+    custom_nth1(RowBelow, Board, RowBelowList),
+    custom_nth1(ColRight, RowBelowList, Elem),
+    ((Elem = w ; Elem = b) ->
+        NewBoard = Board;
+    (Elem = 0) ->  
+        replace_swap(ColRight, RowBelowList, p, NewBottomRight),
+        replace_swap(RowBelow, Board, NewBottomRight, TempBoard),
+        swapDiagonal4(RowBelow, ColRight, TempBoard, NewBoard);
+    NewBoard = Board).
+
+/*------------------ Replace ------------------*/
+% Replace an element at a specific index in a list
+replace_swap(0, [_|Rest], Element, [Element|Rest]).
+
+replace_swap(Index, [X|Rest], Element, [X|NewRest]) :-
+    Index > 0,
+    NewIndex is Index - 1,
+    replace_swap(NewIndex, Rest, Element, NewRest).
+    
+/* Function to replace the ps with zeros */
+clean_playables([], []).
+clean_playables([Row|RestOfRows], [NewRow|NewRest]) :-
+    replace_p_in_row(Row, NewRow),
+    clean_playables(RestOfRows, NewRest).
+
+replace_p_in_row([], []).
+replace_p_in_row([p|Rest], [0|NewRest]) :-
+    replace_p_in_row(Rest, NewRest).
+replace_p_in_row([X|Rest], [X|NewRest]) :-
+    X \= p,
+    replace_p_in_row(Rest, NewRest).
+    
+% Example usage:
+% Define the initial board
+% [[0],[0, 0],[0, b, 0],[0, 0, w, 0],[w, 0, 0, 0, 0],[0, w, 0, 0, 0, 0],[0, w, b, b, w, 0, 0],[0, b, w, w, 0, 0],[0, b, 0, 0, 0],[0, w, w, b],[0, 0, 0],[0, b],[b]]
+
+% ---------------------------------------------------------------------------------------------------
